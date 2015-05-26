da03633565f3501d4885c4db06b08bbd381209c2
David Moles <david.moles@ucop.edu>
2015-05-26 13:36:36 -0700

    Tidied up example

--- example.rb
+++ example.rb
@@ -10,25 +10,26 @@ client = Resync::Client.new
 
 # Note: this URI is from resync-simulator: https://github.com/resync/resync-simulator
 source_desc_uri = 'http://localhost:8888/.well-known/resourcesync'
-
 puts "Source: #{source_desc_uri}"
 source_desc = client.get(source_desc_uri)
-
-desc_ln = source_desc.links.find { |ln| ln.rel == 'describedby' }
+desc_ln = source_desc.link_for(rel: 'describedby')
 puts "  Described by: #{desc_ln.href}"
+puts
 
-cap_list_uri = source_desc.resources.find { |r| r.metadata.capability == 'capabilitylist' }.uri
+cap_list_uri = source_desc.resource_for(capability: 'capabilitylist').uri
 puts "Capability list: #{cap_list_uri}"
+puts
+
 cap_list = client.get(cap_list_uri)
-
-change_list_uri = cap_list.resources.find { |r| r.metadata.capability == 'changelist' }.uri
+change_list_uri = cap_list.resource_for(capability: 'changelist').uri
 puts "Change list: #{change_list_uri}"
-change_list = client.get(change_list_uri)
 
-puts "  from:  #{change_list.metadata.from_time}"
-puts "  until: #{change_list.metadata.until_time}"
+change_list = client.get(change_list_uri)
+puts "  from:    #{change_list.metadata.from_time}"
+puts "  until:   #{change_list.metadata.until_time}"
 changes = change_list.resources
-puts "  #{changes.size} total changes"
+puts "  changes: #{changes.size}"
+puts
 
 last = changes.size > 5 ? 5 : changes.size
 puts "last #{last} changes:"
