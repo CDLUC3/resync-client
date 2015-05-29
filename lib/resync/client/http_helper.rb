527954cb26e4dccd4de2656b6f0d711a616aac46
David Moles <david.moles@ucop.edu>
2015-05-29 13:10:48 -0700

    Fixed HTTPHelper to use read_body correctly, but broke tests

--- lib/resync/client/http_helper.rb
+++ lib/resync/client/http_helper.rb
@@ -31,27 +31,36 @@ module Resync
 
     # Gets the content of the specified URI as a
     # {http://ruby-doc.org/stdlib-2.2.2/libdoc/net/http/rdoc/Net/HTTPResponse.html Net::HTTPResponse}
-    # @return [Net::HTTPResponse] the response, if successful
-    def fetch(uri, limit = redirect_limit)
+    # @param uri [URI] The URI to retrieve
+    # @param limit [Integer] the number of redirects to follow before erroring out
+    # @param block [Block] an optional block to be applied to the response in order to
+    #   be able to invoke +Net::HTTPResponse#read_body+; if this is provided, the return
+    #   value of the method will be the return value of the block. (It's a bit hackish;
+    #   see {http://stackoverflow.com/a/29598327/27358 this Stack Overflow discussion}
+    #   for an explanation of the underlying +Net:HTTP+ issues.)
+    # @return [Net::HTTPResponse] the response, if successful, or the result of the applied
+    #   block
+    def fetch(uri, limit = redirect_limit, &block)
       fail "Redirect limit (#{redirect_limit}) exceeded retrieving URI #{uri}" if limit <= 0
       req = Net::HTTP::Get.new(uri, 'User-Agent' => user_agent)
       Net::HTTP.start(uri.hostname, uri.port, use_ssl: (uri.scheme == 'https')) do |http|
-        handle_response(uri, limit, req, http)
+        handle_response(uri, limit, req, http, &block)
       end
     end
 
     # Downloads the content of the specified URI to a temporary file
     # @return [String] the path to the temporary file
     def fetch_to_file(uri, limit = redirect_limit)
-      response = fetch(uri, limit)
-      tempfile = Tempfile.new(['resync-client', ".#{extension_for(response)}"])
-      begin
-        open tempfile, 'w' do |out|
-          response.read_body { |chunk| out.write(chunk) }
+      fetch(uri, limit) do |response|
+        tempfile = Tempfile.new(['resync-client', ".#{extension_for(response)}"])
+        begin
+          open tempfile, 'w' do |out|
+            response.read_body { |chunk| out.write(chunk) }
+          end
+          return tempfile.path
+        ensure
+          tempfile.close
         end
-        tempfile.path
-      ensure
-        tempfile.close
       end
     end
 
@@ -74,14 +83,16 @@ module Resync
     end
 
     def handle_response(uri, limit, req, http)
-      response = http.request(req)
-      case response
-      when Net::HTTPSuccess
-        response
-      when Net::HTTPInformation, Net::HTTPRedirection
-        fetch(redirect_uri_for(response, uri), limit - 1)
-      else
-        fail "Error #{response.code}: #{response.message} retrieving URI #{uri}"
+      http.request(req) do |response|
+        case response
+        when Net::HTTPSuccess
+          yield response if block_given?
+          response
+        when Net::HTTPInformation, Net::HTTPRedirection
+          fetch(redirect_uri_for(response, uri), limit - 1)
+        else
+          fail "Error #{response.code}: #{response.message} retrieving URI #{uri}"
+        end
       end
     end
 
