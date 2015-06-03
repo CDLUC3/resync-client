d6d454516d1d0fb92fc4b19875bbdee2dbdb98f8
David Moles <david.moles@ucop.edu>
2015-06-03 12:17:00 -0700

    Update to depend on resync 0.1.1

---
+++ CHANGES.md
@@ -0,0 +1,7 @@
+# 0.1.1
+
+- Update to depend on [resync](https://github.com/dmolesUC3/resync) 0.1.1
+
+# 0.1.0
+
+- Initial release
--- lib/resync/client/version.rb
+++ lib/resync/client/version.rb
@@ -1,6 +1,6 @@
 module Resync
   class Client
     # The version of this gem.
-    VERSION = '0.1.0'
+    VERSION = '0.1.1'
   end
 end
